25c5299a2412d9e06885902ec1fa22a9f9997ff7
ForceClose31 <forceclose31@gmail.com>
2024-06-29 19:02:32 +0700

    update all clean

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'petugas_rekam_medis', 'perawat', 'dokter', 'bidan', 'kepala_klinik', 'apoteker']);
             $table->rememberToken();
             $table->timestamps();
         });
